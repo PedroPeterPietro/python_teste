7b61e28600663fb8176096dc2cb34618ce43117e
Pedro Tadeu <84622239+PedroPeterPietro@users.noreply.github.com>
2023-07-06 13:16:28 -0300

    Add files via upload

---
+++ sistema_bancario_def.py
@@ -0,0 +1,64 @@
+import textwrap 
+def main():
+    print('#############Olá, bem-vindo ao banco digital PedroPeterPietro###########')
+    menu = ""
+    quantidade_de_saldo = 0
+    saldo = 0
+    extrato = ""
+
+    while(menu != 4):
+        print("""
+        ############# QUAL AÇÃO VOCÊ DESEJA EXECUTAR? ##################
+        1 - Sacar
+        2 - Depositar
+        3 - Visualizar Extrato
+        4 - Sair
+        """)
+        menu = input()
+        
+        if menu == "1":
+            sacar()
+        
+        elif menu == "2":
+            depositar()
+        elif menu == "3":
+            extrato()
+        elif menu =="4":
+              break;
+
+
+def sacar():
+    global extrato
+    global saldo
+    global quantidade_de_saldo
+    valor = float(input("Informe o valor do saque: "))
+    if valor > 0 and valor <= saldo:
+        if quantidade_de_saldo < 3:
+                    saldo -= valor
+                    extrato += f"Saque: R$ {valor:.2f}\n"
+                    quantidade_de_saldo += 1
+                    print("Saque Concluido!")
+        else:
+                    print("Prezado cliente, você já excedeu o limite de saques diários.")
+    else:
+        print("Prezado cliente, o valor digitado é menor que o saldo disponível. Favor tentar novamente.")   
+
+def depositar():
+    global extrato
+    global saldo
+    global quantidade_de_saldo
+
+    valor = float(input("Informe o valor do depósito: "))
+    if valor > 0:
+                saldo += valor
+                extrato += f"Depósito: R$ {valor:.2f}\n"
+                quantidade_de_saldo += 1
+                print("Depósito Concluido!")
+    else:
+                print("Prezado cliente, o valor digitado não é válido.")
+        
+def verificar_extrato():
+    global extrato
+    print(f"Esse é o extrato {extrato}")
+
+main()
